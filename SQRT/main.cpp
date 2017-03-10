8c656f10ff66e9446eb4bd9b79558664713afc3c
Simon <simonkalle@hotmail.com>
2017-03-10 22:29:36 +0100

    added random (buggy) and some comments

--- SQRT/main.cpp
+++ SQRT/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <math.h>
-using namespace std;
-double Magic(double m1, double m2);
-void Test(void);
+#include <stdlib.h>
+#include <ctime>
+using namespace std;                                        //used by cin and cout. basc stuff.
+double Magic(double m1, double m2);                         //placeholder. apparently neccesarry.
+void Test(void);                                            //another placeholder.
 
 int main(void)
 {
+    Test();
     double MagicNumber, FirstNumber, SecondNumber,MagicRoot;
     cout<< "please enter a magic number"<<endl;
     cin>>FirstNumber;
@@ -18,17 +21,19 @@ int main(void)
     cout<<"The root of magic is "<< MagicRoot<<endl;
     MagicRoot=Magic(FirstNumber,SecondNumber);
     cout<<"The root of magic is "<< MagicRoot<<endl;
-
     return 0;
 }
 
 double Magic(double m1, double m2)
 {
+
     double result=sqrt(m1+m2)+1;
     return(result);
 }
 
 void Test(void)
 {
-
+    srand(time(NULL));                          //find out why this is an error. and how it works...
+    double v1=rand();
+    cout<<"randoms:"<< v1<<endl;                //yay. I mae fire. ...I mean a rondom. wonder how big it can get?
 }

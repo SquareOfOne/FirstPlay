502a0441f0c370426c841258cbcd0b68a39ca369
Simon <simonkalle@hotmail.com>
2017-03-12 23:10:14 +0100

    forfill lowest requirements, with a huge bug. prove of concept

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-
+//TODO: make array dynamic, wait for commands, add text commands, add UI.
 class UseArray{
-
 private:
-    int lenght;
+    int length;                     //used by for loop. fix: dynamicArray[lenght]
+    int * plength;                  //pointer to change length.
     int dynamicArray[10];           //temporary fix
 
 public:
@@ -19,30 +19,31 @@ int main(void)
     UseArray bestArray;
     int command;
     cin>>command;
-    if (command==0){            //for now, 0 is the command used to add numbers.
+    if (command==0){                //for now, 0 is the command used to add numbers.only works once.have to be used.FIX.
         int input=0;
         cout<<"Enter Number"<<endl;
         cin>>input;
-        bestArray.addNumber(input);
+        bestArray.addNumber(input); //this might have to be a pointert as well..
     }
     cout<< bestArray.findBig();
     return 0;
 }
 
 UseArray::UseArray(){
-    lenght=1;           //this actually means the lenght is 1.
+    length=1;                       //set length to 1.
     dynamicArray[0]=1;
+    plength= &length;
+
 }
 
 int UseArray::addNumber(int n){
-    lenght=lenght+1;
-    return dynamicArray[lenght]=n;
+    *plength=length+1;
+    return dynamicArray[length]=n;  //bug: number is not saved to "bestArray". random int take the spot.
 }
 
-int UseArray::findBig(void){
-    int highest;
-    for (int i=0; i>lenght; i++){
-
+int UseArray::findBig(void){        //at least this one seems to work.
+    int highest;                    //smallest number is basically the same. why i did not bother.
+    for (int i=0; i<length; i++){
         if (i==0){
            highest=dynamicArray[i];
         }

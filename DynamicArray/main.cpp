5248a97a9c4060673f0979da57acb4820df6a45c
Simon <simonkalle@hotmail.com>
2017-03-12 22:33:45 +0100

    buggy sorting, simple input in array program

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -5,25 +5,50 @@ class UseArray{
 
 private:
     int lenght;
-    int dynamicArray[lenght];
-
+    int dynamicArray[10];           //temporary fix
 
 public:
-    addNumber(int n);
-    findBig();
-    findSmall();
+    UseArray ();
+    int addNumber(int n);
+    int findBig(void);
+    int findSmall(void);
 };
 
 int main(void)
 {
     UseArray bestArray;
-
-
-    cout << "Hello World!" << endl;
+    int command;
+    cin>>command;
+    if (command==0){            //for now, 0 is the command used to add numbers.
+        int input=0;
+        cout<<"Enter Number"<<endl;
+        cin>>input;
+        bestArray.addNumber(input);
+    }
+    cout<< bestArray.findBig();
     return 0;
 }
 
 UseArray::UseArray(){
-    lenght=1;
+    lenght=1;           //this actually means the lenght is 1.
     dynamicArray[0]=1;
+}
+
+int UseArray::addNumber(int n){
+    lenght=lenght+1;
+    return dynamicArray[lenght]=n;
+}
+
+int UseArray::findBig(void){
+    int highest;
+    for (int i=0; i>lenght; i++){
+
+        if (i==0){
+           highest=dynamicArray[i];
+        }
+        else if (highest<dynamicArray[i]){
+        highest=dynamicArray[i];
+        }
+    }
+  return highest;
 }

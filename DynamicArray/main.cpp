920a8b93a7aecb961be3af5fd947ece53d51ea92
Simon <simonkalle@hotmail.com>
2017-03-12 21:24:04 +0100

    added a program for arrays

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[])
+class UseArray{
+
+private:
+    int lenght;
+    int dynamicArray[lenght];
+
+
+public:
+    addNumber(int n);
+    findBig();
+    findSmall();
+};
+
+int main(void)
 {
+    UseArray bestArray;
+
+
     cout << "Hello World!" << endl;
     return 0;
+}
+
+UseArray::UseArray(){
+    lenght=1;
+    dynamicArray[0]=1;
 }

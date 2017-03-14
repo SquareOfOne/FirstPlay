8226f99d7ac959903fdc7a7d7a6d990e5498002d
Simon <simonkalle@hotmail.com>
2017-03-14 11:51:21 +0100

    Add method works. Command method added

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -1,55 +1,25 @@
-#include <iostream>
-using namespace std;
-//TODO: make array dynamic, wait for commands, add text commands, add UI.
-class UseArray{
-private:
-    int length;                     //used by for loop. fix: dynamicArray[lenght]
-    int * plength;                  //pointer to change length.
-    int dynamicArray[10];           //temporary fix
-
-public:
-    UseArray ();
-    int addNumber(int n);
-    int findBig(void);
-    int findSmall(void);
-};
+#include <iostream>                 //Basic input/output.
+#include <string>                   //Allow the user to give text commands.
+#include <numbers.h>                //Add array class.
+using namespace std;                // for cin and cout.
+                                    //TODO: make array dynamic, wait for commands, add text commands, add UI.
 
 int main(void)
 {
-    UseArray bestArray;
-    int command;
+    Numbers bestArray;
+    string command;
+    while (true){
     cin>>command;
-    if (command==0){                //for now, 0 is the command used to add numbers.only works once.have to be used.FIX.
-        int input=0;
-        cout<<"Enter Number"<<endl;
-        cin>>input;
-        bestArray.addNumber(input); //this might have to be a pointert as well..
+    if (command=="add")
+        bestArray.addNumber();
+    if (command=="print")
+        bestArray.print();
+    if (command=="quit")
+        break;
+    cout<< "Enter new command\n";
     }
-    cout<< bestArray.findBig();
     return 0;
 }
 
-UseArray::UseArray(){
-    length=1;                       //set length to 1.
-    dynamicArray[0]=1;
-    plength= &length;
 
-}
 
-int UseArray::addNumber(int n){
-    *plength=length+1;
-    return dynamicArray[length]=n;  //bug: number is not saved to "bestArray". random int take the spot.
-}
-
-int UseArray::findBig(void){        //at least this one seems to work.
-    int highest;                    //smallest number is basically the same. why i did not bother.
-    for (int i=0; i<length; i++){
-        if (i==0){
-           highest=dynamicArray[i];
-        }
-        else if (highest<dynamicArray[i]){
-        highest=dynamicArray[i];
-        }
-    }
-  return highest;
-}
--- DynamicArray/numbers.cpp
+++ DynamicArray/numbers.cpp
@@ -1,19 +1,35 @@
 #include "numbers.h"
+#include <iostream>
 using namespace std;
 Numbers::Numbers()
 {
 
 }
-Numbers::addNumber(){
-    int addUp;
+void Numbers::addNumber(){
     cout<<"How many numbers would you like to add?\n";
-    cin>>addUp;
-    numberOfNumbers=numberOfNumbers+addUp;
-    for (int i=0;i<addUp;i++)
-        cin>>numberRow[numberOfNumbers+i];
+    cin>>numberOfNumbers;
+    numberRow= new int[numberOfNumbers];
+    for (int i=0;i<numberOfNumbers;i++)
+        cin>>numberRow[numberOfNumbers];
 }
 
-Numbers::print(){
-    for (int i=0;numberOfNumbers;i++)
-        cout<<"Number "<< i<< "is" <<numberRow[i]<<endl;
+void Numbers::print(){
+    for (int i=0;i<numberOfNumbers;i++)
+        cout<<"Number "<< i<< " is " <<numberRow[i]<<"\n";
 }
+
+
+/*
+int UseArray::findBig(void){        //at least this one seems to work.
+    int highest;                    //smallest number is basically the same. why i did not bother.
+    for (int i=0; i<length; i++){
+        if (i==0){
+           highest=dynamicArray[i];
+        }
+        else if (highest<dynamicArray[i]){
+        highest=dynamicArray[i];
+        }
+    }
+  return highest;
+}
+*/
